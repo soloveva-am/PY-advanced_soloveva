85b63e329e23986db9609572f2196de32c73c060
anna <soloveva.am@phystech.edu>
2021-10-25 19:34:08 +0300

    13

--- src/iters_25.py
+++ src/iters_25.py
@@ -1,17 +1,23 @@
-from typing import List, Any
+
 
 print('N1')
+
+
 def print_map(function, iterable):
-    i=iter(iterable)
+    i1 = iter(iterable)
 
     while True:
-        try: print(function(next(i)))
-        except StopIteration: break
+        try:
+            print(function(next(i1)))
+        except StopIteration:
+            break
 print_map(float, range(5))
 
 print('N2')
+
+
 def fib_range(n, a=0, b=1):
-    for i in range (n):
+    for i2 in range (n):
         yield b
         a,b= b, (b+a)
 
@@ -20,6 +26,8 @@ for i in fib_range(6):
 
 
 print('N3')
+
+
 def my_map(function, iterable):
     i=iter(iterable)
 
@@ -27,6 +35,7 @@ def my_map(function, iterable):
         try: e=next(i)
         except StopIteration: break
         yield function(e)
+
 
 def my_zip(*iterables):
     L=[]
@@ -41,35 +50,46 @@ def my_zip(*iterables):
         except StopIteration:
             break
 
+
 def my_enumerate(iterable):
     i=0
     for j in iterable:
         yield (i, j)
         i+=1
 
+
 for i in my_map(float, range(5)):
     print(i)
 print(' ')
 
+
 for i in my_enumerate( range(5)):
     print(i)
 print(' ')
+
 for i in my_zip([1, 2, 3], [2, 3, 4], ['a','b','c']):
     print(i)
 print(' ')
 print('N4')
+
 import itertools
 print('N4.1')
+
+
 def get_cartesian_product(a, b):
     S=itertools.product(a,b,repeat=1)
     return list(S)
 print(get_cartesian_product([1,2], [5,6,7]))
 
 print('N4.2')
+
+
 def get_permutations(s, n):
     return sorted(itertools.permutations(s,n))
 print(get_permutations('fgahk', 3))
 print('N4.3')
+
+
 def get_combinations(s,n):
     S=[]
     for i in range(1,n+1):
@@ -78,10 +98,14 @@ def get_combinations(s,n):
     return S
 print(get_combinations('fgahk', 3))
 print('N4.4')
+
+
 def get_cwr(s,n):
     return list (itertools.combinations_with_replacement(s,n))
 print(get_cwr('kgjf',2))
 print('N4.5')
+
+
 def compress_string(s):
     S=[]
     for i in itertools.groupby(s):
@@ -92,6 +116,8 @@ def compress_string(s):
     return(S)
 print(compress_string('ggghhdddgg'))
 print('N4.6')
+
+
 def maxvalue(K, m):
     M=0
     for i in itertools.product(*K):
