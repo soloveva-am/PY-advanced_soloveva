3c81335263479f985bd4b5985914c20159681990
anna <soloveva.am@phystech.edu>
2021-11-15 19:34:22 +0300

    вот мультитрединг

--- src/multi_15_11.py
+++ src/multi_15_11.py
@@ -152,3 +152,26 @@ print(time() - start)
 
 
 print('#5')
+# because list in process is instantly local while it's not stated global
+import multiprocessing
+
+
+def worker():
+    LIST.append('item')
+
+
+LIST = []
+
+
+if __name__ == "__main__":
+    processes = [
+        multiprocessing.Process(target=worker)
+        for _ in range(5)
+    ]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+    print(LIST)
+
+print('#6')
