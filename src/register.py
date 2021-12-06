f0b4693a87e260ffd77a0b2c05033c7a7b368a12
anna <soloveva.am@phystech.edu>
2021-12-06 19:31:28 +0300

    вот дерево fixed doubles.

--- src/register.py
+++ src/register.py
@@ -3,7 +3,23 @@ import socket                            # Подключаем
 sock = socket.socket()                   # Создаём
 sock.connect(("81.200.31.248", 9000))    # Присоединяемся
 
-sock.send("register".encode())     # Отправка
+#sock.send("register".encode())     # Отправка
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
+sock.sendall("Register".encode())     # Отправка
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
+sock.sendall('A47'.encode())
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
+sock.sendall('Solovyeva Anna Michailovna'.encode())
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
+sock.sendall('B03-007'.encode())
 data = sock.recv(1000) # Ответ
 data = data.decode("utf8")
 print(data)
---
+++ src/secret key.py
@@ -0,0 +1,37 @@
+import socket                            # Подключаем
+import gzip
+sock = socket.socket()                   # Создаём
+sock.connect(("81.200.31.248", 9000))    # Присоединяемся
+
+#sock.send("register".encode())     # Отправка
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
+sock.sendall("Get secret key".encode())     # Отправка
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
+sock.sendall("A47".encode())     # Отправка
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
+sock.sendall("about".encode())     # Отправка
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
+sock.sendall("next".encode())     # Отправка
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
+"""Для архивации данных применяется библиотека gzip
+метод gzip.compress(data) архивирует (сжимает) байтстроку data
+метод gzip.decompress(data) разархивирует"""
+sock.sendall("help".encode())     # Отправка
+data = sock.recv(1000) # Ответ
+data = gzip.decompress(data)
+data = data.decode("utf8")
+print(data)
+sock.sendall(gzip.compress("next".encode()))     # Отправка
+data = sock.recv(1000) # Ответ
+data = data.decode("utf8")
+print(data)
